973c1dcfad5615b7bde8900f39dc8e13010c1b72
SamK <101374@glr.nl>
2025-01-08 22:55:14 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+// Instellen van include path
+define('VIEW_PATH', __DIR__ . '/view');
+define('CONFIG_PATH', __DIR__ . '/config.php');
+define('UTILS_PATH', __DIR__ . '/phpUtils.php');
+
+// Laad configuratie en utilities
+require_once CONFIG_PATH;
+require_once UTILS_PATH;
+
+// Laad de view
+require_once VIEW_PATH . '/index_view.php';
+?>
